51e7d1bb52c8e7795bf227156d51708b7ebf8485
Dorychop Riddle <cmriddle369@gmail.com>
2020-04-02 18:39:28 -0400

    struggling

--- src/components/contact/contact-form.js
+++ src/components/contact/contact-form.js
@@ -66,7 +66,7 @@ export default class ContactForm extends Component {
     render() {
         return (
             <div className="contact-form-wrapper">
-                <form action="https://cmriddle369@gmail.com" method="GET" onSubmit={this.handleSubmit} className="form-wrapper">
+                <form onSubmit={this.handleSubmit} className="form-wrapper">
                     <h3>Please fill the form out and we will get back to you as soon as possible.</h3>
                     <div className="form">
                         <label><strong>Name: </strong></label>
